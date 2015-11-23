e0b14a0abbdd929f3bb263ee4b8bd1b769db6633
fheslouin <florian.heslouin@zimdeo.fr>
2015-11-23 10:47:22 +0100

    switch back vars, bug fix on role

--- oneClickDeploy.sh
+++ oneClickDeploy.sh
@@ -8,4 +8,4 @@ sudo mkdir -p /var/lib/ansible/local
 cd /var/lib/ansible/
 git clone https://github.com/ideascube/ansiblecube.git local
 sudo cp /var/lib/ansible/local/hosts /etc/ansible/hosts
-/usr/local/bin/ansible-pull -d /var/lib/ansible/local -i hosts -U https://github.com/ideascube/ansiblecube.git serveurInstall.yml
+/usr/local/bin/ansible-pull -d /var/lib/ansible/local -i hosts -U https://github.com/ideascube/ansiblecube.git serveurInstall.yml --extra-vars "use_hdd=False send_to_central_server=False import_ideascube_data=False"
