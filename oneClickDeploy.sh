05f3557c4ff4e2542e29f19c9b686e81355bf7cc
fheslouin <florian.heslouin@zimdeo.fr>
2015-11-23 10:39:39 +0100

    update variables, disable sudo

--- oneClickDeploy.sh
+++ oneClickDeploy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-sudo apt-get update 
+sudo apt-get update
 sudo apt-get install -y python-pip git python-dev
-sudo pip install ansible markupsafe 
-echo "localhost ansible_connection=local" > /etc/ansible/hosts
-cd /tmp/
-git clone https://github.com/ideascube/ansiblecube.git 
-cd ansiblecube/ 
+sudo pip install ansible markupsafe
+sudo mkdir /etc/ansible
+sudo mkdir -p /var/lib/ansible/local
+cd /var/lib/ansible/
+git clone https://github.com/ideascube/ansiblecube.git local
+sudo cp /var/lib/ansible/local/hosts /etc/ansible/hosts
 /usr/local/bin/ansible-pull -d /var/lib/ansible/local -i hosts -U https://github.com/ideascube/ansiblecube.git serveurInstall.yml
