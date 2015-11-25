995bf5c5257fb2db64bb3604134e2f565a761295
fheslouin <florian.heslouin@zimdeo.fr>
2015-11-25 13:38:43 +0100

    Generate SSH Key to upload info to central serveur

--- oneClickDeploy.sh
+++ oneClickDeploy.sh
@@ -7,5 +7,7 @@ sudo mkdir /etc/ansible
 sudo mkdir -p /var/lib/ansible/local
 cd /var/lib/ansible/
 git clone https://github.com/ideascube/ansiblecube.git local
+ssh-keygen -t rsa -b 4096 -C "it@bibliosansfrontieres.org"
+ssh-copy-id ansible@37.187.151.52
 sudo cp /var/lib/ansible/local/hosts /etc/ansible/hosts
-/usr/local/bin/ansible-pull -d /var/lib/ansible/local -i hosts -U https://github.com/ideascube/ansiblecube.git serveurInstall.yml --extra-vars "use_hdd=False send_to_central_server=False import_ideascube_data=False download_data=False"
+/usr/local/bin/ansible-pull -d /var/lib/ansible/local -i hosts -U https://github.com/ideascube/ansiblecube.git serveurInstall.yml --extra-vars "use_hdd=Falses send_to_central_server=True import_ideascube_data=False download_data=True projectName=$1"
