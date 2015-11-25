a9f74c745c8cb7547ae3feb36baf8b951040cd3a
fheslouin <florian.heslouin@zimdeo.fr>
2015-11-25 14:36:17 +0100

    change wpa password and bug fix

--- oneClickDeploy.sh
+++ oneClickDeploy.sh
@@ -10,4 +10,4 @@ git clone https://github.com/ideascube/ansiblecube.git local
 echo -e "\n\n\n" | ssh-keygen -t rsa -f /root/.ssh/id_rsa -b 4096 -C "it@bibliosansfrontieres.org" -N ""
 ssh-copy-id -o StrictHostKeyChecking=no ansible@37.187.151.52 
 sudo cp /var/lib/ansible/local/hosts /etc/ansible/hosts
-/usr/local/bin/ansible-pull -d /var/lib/ansible/local -i hosts -U https://github.com/ideascube/ansiblecube.git serveurInstall.yml --extra-vars "use_hdd=Falses send_to_central_server=True import_ideascube_data=False download_data=True projectName=$1"
+/usr/local/bin/ansible-pull -d /var/lib/ansible/local -i hosts -U https://github.com/ideascube/ansiblecube.git serveurInstall.yml --extra-vars "use_hdd=False send_to_central_server=True import_ideascube_data=False download_data=True projectName=$1"
