23bec1f241b0749937cf7119f4ce6c642f6b53c3
fheslouin <florian.heslouin@zimdeo.fr>
2015-11-30 12:23:41 +0100

    move network manager install to ideascube role

--- oneClickDeploy.sh
+++ oneClickDeploy.sh
@@ -1,5 +1,6 @@
  #!/bin/bash
 SHOULD_WE_SEND="False"
+SSH_KEY="/root/.ssh/id_rsa" 
 
 sudo apt-get update
 sudo apt-get install -y python-pip git python-dev
@@ -9,10 +10,12 @@ sudo mkdir -p /var/lib/ansible/local
 cd /var/lib/ansible/
 git clone https://github.com/ideascube/ansiblecube.git local
 
-if [ "$1" = sync ]; then
+if [ "$1" = sync ] && [ ! -f "$SSH_KEY" ]; then
 	SHOULD_WE_SEND="True"
 	echo -e "\n\n\n" | ssh-keygen -t rsa -f /root/.ssh/id_rsa -b 4096 -C "it@bibliosansfrontieres.org" -N ""
 	ssh-copy-id -o StrictHostKeyChecking=no ansible@37.187.151.52 
+else
+	SHOULD_WE_SEND="True"
 fi
 
 sudo cp /var/lib/ansible/local/hosts /etc/ansible/hosts
