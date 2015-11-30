f4c40a8855d38f64bd938da2fbb9974050e0d610
fheslouin <florian.heslouin@zimdeo.fr>
2015-11-30 10:21:55 +0100

    Optimize playbook

--- oneClickDeploy.sh
+++ oneClickDeploy.sh
@@ -1,5 +1,5 @@
  #!/bin/bash
-should_we_send = False
+SHOULD_WE_SEND="False"
 
 sudo apt-get update
 sudo apt-get install -y python-pip git python-dev
@@ -10,10 +10,10 @@ cd /var/lib/ansible/
 git clone https://github.com/ideascube/ansiblecube.git local
 
 if [ "$1" = sync ]; then
-	should_we_send = True
+	SHOULD_WE_SEND="True"
 	echo -e "\n\n\n" | ssh-keygen -t rsa -f /root/.ssh/id_rsa -b 4096 -C "it@bibliosansfrontieres.org" -N ""
 	ssh-copy-id -o StrictHostKeyChecking=no ansible@37.187.151.52 
 fi
 
 sudo cp /var/lib/ansible/local/hosts /etc/ansible/hosts
-/usr/local/bin/ansible-pull -d /var/lib/ansible/local -i hosts -U https://github.com/ideascube/ansiblecube.git serveurInstall.yml --extra-vars "use_hdd=False send_to_central_server=$should_we_send import_ideascube_data=False download_data=True ideascube_id=$2 timezone=$3"
+/usr/local/bin/ansible-pull -d /var/lib/ansible/local -i hosts -U https://github.com/ideascube/ansiblecube.git -C ideasbox globalInstall.yml --extra-vars "use_hdd=True send_to_central_server="$SHOULD_WE_SEND" import_ideascube_data=False download_data=False ideascube_id=$2 timezone=$3"
