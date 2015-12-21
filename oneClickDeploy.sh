d26b9dd9c5a796adad7a52bc2c761509dc19d78a
fheslouin <florian.heslouin@zimdeo.fr>
2015-12-21 16:56:13 +0100

    Update oneClickDeploy.sh with more understandable parameters and disable all install on conf file

--- oneClickDeploy.sh
+++ oneClickDeploy.sh
@@ -1,29 +1,56 @@
- #!/bin/bash
+#!/bin/bash
 SHOULD_WE_SEND="False"
 SSH_KEY="/root/.ssh/id_rsa" 
 
-if [ "$1" = sync ] && [ ! -f "$SSH_KEY" ]; then
+send_to_central_server=`echo $1 | cut -d= -f1`
+value1=`echo $1 | cut -d= -f2`
+
+ideascube_id=`echo $2 | cut -d= -f1`
+value2=`echo $2 | cut -d= -f2`
+
+timezone=`echo $3 | cut -d= -f1`
+value3=`echo $3 | cut -d= -f2`
+
+download_data=`echo $4 | cut -d= -f1`
+value4=`echo $4 | cut -d= -f2`
+
+echo "$send_to_central_server"
+
+
+if [ "$send_to_central_server" = send_to_central_server ] && [ "$value1" = True ] && [ ! -f "$SSH_KEY" ]; then
 
 	SHOULD_WE_SEND="True"
 	echo -e "\n\n\n" | ssh-keygen -t rsa -f /root/.ssh/id_rsa -b 4096 -C "it@bibliosansfrontieres.org" -N ""
 	ssh-copy-id -o StrictHostKeyChecking=no ansible@37.187.151.52 
 
-elif [ "$1" = sync ] && [ -f "$SSH_KEY" ]; then
+elif [ "$send_to_central_server" = send_to_central_server ] && [ "$value1" = True ] && [ -f "$SSH_KEY" ]; then
 
 	SHOULD_WE_SEND="True"
 
-else
+elif [ "$send_to_central_server" = "send_to_central_server" ] && [ "$value1" = False ]; then
 
 	SHOULD_WE_SEND="False"
+else
+	echo -e "
+	YOU HAVE TO BE ROOT TO LUNCH THIS SCRIPT !
+
+	Usage : ./oneClickDeploy.sh send_to_central_server=True ideascube_id=kb_mooc_cog timezone=Europe/Paris download_data=True
+
+	send_to_central_server=True|False : Wether send or not some system information to central server
+	ideascube_id=File_Name : Must be the same name as the one used for the ideascube configuration file
+	timezone=Europe/Paris : Is the timezone 
+	download_data=True|False : Wether download or not data from ZIM project (ex : wikipedia zim file, etc.)
+	"
+	exit 0;
 fi
 
-sudo apt-get update
-sudo apt-get install -y python-pip git python-dev
-sudo pip install ansible markupsafe
-sudo mkdir /etc/ansible
-sudo mkdir -p /var/lib/ansible/local
+apt-get update
+apt-get install -y python-pip git python-dev
+pip install ansible markupsafe
+mkdir /etc/ansible
+mkdir -p /var/lib/ansible/local
 cd /var/lib/ansible/
 git clone https://github.com/ideascube/ansiblecube.git local
 
-sudo cp /var/lib/ansible/local/hosts /etc/ansible/hosts
-/usr/local/bin/ansible-pull -d /var/lib/ansible/local -i hosts -U https://github.com/ideascube/ansiblecube.git globalInstall.yml --extra-vars "send_to_central_server="$SHOULD_WE_SEND" import_ideascube_data=False download_data=$4 ideascube_id=$2 timezone=$3"
+cp /var/lib/ansible/local/hosts /etc/ansible/hosts
+/usr/local/bin/ansible-pull -d /var/lib/ansible/local -i hosts -U https://github.com/ideascube/ansiblecube.git globalInstall.yml --extra-vars "send_to_central_server="$SHOULD_WE_SEND" import_ideascube_data=False ideascube_id=$value2 timezone=$value3 download_data=$value4"
