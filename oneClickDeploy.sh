38e25f836c0d39bd093c4c67c0b140b31675673e
fheslouin <florian.heslouin@zimdeo.fr>
2016-03-30 08:46:12 +0200

    Rename send_to_central_server in sync_log for easier understanding and remove unused variables

--- oneClickDeploy.sh
+++ oneClickDeploy.sh
@@ -2,7 +2,7 @@
 SHOULD_WE_SEND="False"
 SSH_KEY="/root/.ssh/id_rsa" 
 
-send_to_central_server=`echo $1 | cut -d= -f1`
+sync_log=`echo $1 | cut -d= -f1`
 value1=`echo $1 | cut -d= -f2`
 
 ideascube_id=`echo $2 | cut -d= -f1`
@@ -11,35 +11,31 @@ value2=`echo $2 | cut -d= -f2`
 timezone=`echo $3 | cut -d= -f1`
 value3=`echo $3 | cut -d= -f2`
 
-download_data=`echo $4 | cut -d= -f1`
-value4=`echo $4 | cut -d= -f2`
-
-echo "$send_to_central_server"
+echo "$sync_log"
 
 
-if [ "$send_to_central_server" = send_to_central_server ] && [ "$value1" = True ] && [ ! -f "$SSH_KEY" ]; then
+if [ "$sync_log" = sync_log ] && [ "$value1" = True ] && [ ! -f "$SSH_KEY" ]; then
 
 	SHOULD_WE_SEND="True"
 	echo -e "\n\n\n" | ssh-keygen -t rsa -f /root/.ssh/id_rsa -b 4096 -C "it@bibliosansfrontieres.org" -N ""
 	ssh-copy-id -o StrictHostKeyChecking=no ansible@37.187.151.52 
 
-elif [ "$send_to_central_server" = send_to_central_server ] && [ "$value1" = True ] && [ -f "$SSH_KEY" ]; then
+elif [ "$sync_log" = sync_log ] && [ "$value1" = True ] && [ -f "$SSH_KEY" ]; then
 
 	SHOULD_WE_SEND="True"
 
-elif [ "$send_to_central_server" = "send_to_central_server" ] && [ "$value1" = False ]; then
+elif [ "$sync_log" = "sync_log" ] && [ "$value1" = False ]; then
 
 	SHOULD_WE_SEND="False"
 else
 	echo -e "
 	YOU HAVE TO BE ROOT TO LUNCH THIS SCRIPT !
 
-	Usage : ./oneClickDeploy.sh send_to_central_server=True ideascube_id=kb_mooc_cog timezone=Europe/Paris download_data=True
+	Usage : ./oneClickDeploy.sh sync_log=True ideascube_id=kb_mooc_cog timezone=Europe/Paris
 
-	send_to_central_server=True|False : Wether send or not some system information to central server
+	sync_log=True|False : Wether send or not log system to a central server. A server with SSH access is required in this case
 	ideascube_id=File_Name : Must be the same name as the one used for the ideascube configuration file
-	timezone=Europe/Paris : Is the timezone 
-	download_data=True|False : Wether download or not data from ZIM project (ex : wikipedia zim file, etc.)
+	timezone=Europe/Paris : The timezone
 	"
 	exit 0;
 fi
@@ -58,6 +54,6 @@ git clone https://github.com/ideascube/ansiblecube.git local
 cp /var/lib/ansible/local/hosts /etc/ansible/hosts
 
 echo "[+] Run globalInstall playbook..."
-/usr/local/bin/ansible-pull -d /var/lib/ansible/local -i hosts -U https://github.com/ideascube/ansiblecube.git globalInstall.yml --extra-vars "send_to_central_server="$SHOULD_WE_SEND" import_ideascube_data=False ideascube_id=$value2 timezone=$value3 download_data=$value4"
+/usr/local/bin/ansible-pull -d /var/lib/ansible/local -i hosts -U https://github.com/ideascube/ansiblecube.git globalInstall.yml --extra-vars "sync_log="$SHOULD_WE_SEND" ideascube_id=$value2 timezone=$value3"
 
 echo "[+] Done."
