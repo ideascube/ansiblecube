c69fb14bda98d3e4daed159c874a47d15b0d98e2
fheslouin <florian.heslouin@zimdeo.fr>
2016-05-26 18:43:19 +0200

    Use more comprehensible variable

--- oneClickDeploy.sh
+++ oneClickDeploy.sh
@@ -2,7 +2,7 @@
 SHOULD_WE_SEND="False"
 SSH_KEY="/root/.ssh/id_rsa" 
 
-sync_log=`echo $1 | cut -d= -f1`
+manage_by_bsf=`echo $1 | cut -d= -f1`
 value1=`echo $1 | cut -d= -f2`
 
 ideascube_project_name=`echo $2 | cut -d= -f1`
@@ -11,29 +11,29 @@ value2=`echo $2 | cut -d= -f2`
 timezone=`echo $3 | cut -d= -f1`
 value3=`echo $3 | cut -d= -f2`
 
-echo "$sync_log"
+echo "$manage_by_bsf"
 
 
-if [ "$sync_log" = sync_log ] && [ "$value1" = True ] && [ ! -f "$SSH_KEY" ]; then
+if [ "$manage_by_bsf" = manage_by_bsf ] && [ "$value1" = True ] && [ ! -f "$SSH_KEY" ]; then
 
 	SHOULD_WE_SEND="True"
 	echo -e "\n\n\n" | ssh-keygen -t rsa -f /root/.ssh/id_rsa -b 4096 -C "it@bibliosansfrontieres.org" -N ""
 	ssh-copy-id -o StrictHostKeyChecking=no ansible@37.187.151.52 
 
-elif [ "$sync_log" = sync_log ] && [ "$value1" = True ] && [ -f "$SSH_KEY" ]; then
+elif [ "$manage_by_bsf" = manage_by_bsf ] && [ "$value1" = True ] && [ -f "$SSH_KEY" ]; then
 
 	SHOULD_WE_SEND="True"
 
-elif [ "$sync_log" = "sync_log" ] && [ "$value1" = False ]; then
+elif [ "$manage_by_bsf" = "manage_by_bsf" ] && [ "$value1" = False ]; then
 
 	SHOULD_WE_SEND="False"
 else
 	echo -e "
 	YOU HAVE TO BE ROOT TO LUNCH THIS SCRIPT !
 
-	Usage : ./oneClickDeploy.sh sync_log=True ideascube_project_name=kb_mooc_cog timezone=Europe/Paris
+	Usage : ./oneClickDeploy.sh manage_by_bsf=True ideascube_project_name=kb_mooc_cog timezone=Europe/Paris
 
-	sync_log=True|False : Whether send or not log system to a central server. A server with SSH access is required in this case
+	manage_by_bsf=True|False : Whether send or not log system to a central server. A server with SSH access is required in this case
 	ideascube_project_name=File_Name : Must be the same name as the one used for the ideascube configuration file
 	timezone=Europe/Paris : The timezone
 	"
@@ -43,7 +43,7 @@ fi
 echo "[+] Install ansible..."
 apt-get update
 apt-get install -y python-pip git python-dev
-pip install ansible==1.9.4 markupsafe
+pip install ansible markupsafe
 
 echo "[+] Clone ansiblecube repo..."
 mkdir --mode 0755 -p /var/lib/ansible/local
@@ -54,6 +54,6 @@ git clone https://github.com/ideascube/ansiblecube.git local
 cp /var/lib/ansible/local/hosts /etc/ansible/hosts
 
 echo "[+] Run globalInstall playbook..."
-/usr/local/bin/ansible-pull -d /var/lib/ansible/local -i hosts -U https://github.com/ideascube/ansiblecube.git globalInstall.yml --extra-vars "sync_log="$SHOULD_WE_SEND" ideascube_project_name=$value2 timezone=$value3"
+/usr/local/bin/ansible-pull -d /var/lib/ansible/local -i hosts -U https://github.com/ideascube/ansiblecube.git globalInstall.yml --extra-vars "manage_by_bsf="$SHOULD_WE_SEND" ideascube_project_name=$value2 timezone=$value3"
 
 echo "[+] Done."
