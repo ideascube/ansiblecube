1dc9b5b0d0f25d050c29d409148f8b06b1ffbfac
fheslouin <florian.heslouin@zimdeo.fr>
2016-06-21 09:03:15 +0200

    Script use to initialize a server

---
+++ oneClickMaster.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "[+] Install ansible..."
+apt-get update
+apt-get install -y python-pip git python-dev
+pip install ansible==2.0 markupsafe
+
+echo "[+] Clone ansiblecube repo..."
+mkdir --mode 0755 -p /var/lib/ansible/local
+cd /var/lib/ansible/
+git clone https://github.com/ideascube/ansiblecube-create-master.git local
+
+[ ! -d /etc/ansible ] && mkdir /etc/ansible
+cp /var/lib/ansible/local/hosts /etc/ansible/hosts
+
+echo "[+] Run globalInstall playbook..."
+/usr/local/bin/ansible-pull -C oneUpdateFile -d /var/lib/ansible/local -i hosts -U https://github.com/ideascube/ansiblecube.git main.yml --tags "master"
+
+echo "[+] Done."
