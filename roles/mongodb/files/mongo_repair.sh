b4f885b829075f877bf75a74bf7fb1e168c38fd4
fheslouin <florian.heslouin@bibliosansfrontieres.org>
2016-08-12 09:45:32 +0200

    Remove log file too

--- roles/mongodb/files/mongo_repair.sh
+++ roles/mongodb/files/mongo_repair.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 if [ -f /media/hdd/mongodb/mongod.lock ]; then
 	rm /media/hdd/mongodb/mongod.lock 
+	rm /var/log/mongodb/mongodb.log
 	/usr/bin/mongod --dbpath=/media/hdd/mongodb --repair
 	chown -R mongodb:nogroup /media/hdd/mongodb
 	chown -R mongodb:nogroup /var/log/mongodb
