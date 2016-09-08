57a953429c8ec8cbbd466941f781f9c93e4ca02a
fheslouin <florian.heslouin@bibliosansfrontieres.org>
2016-09-08 15:38:48 +0200

    Few improvements

--- roles/kiwix/files/create_zim_archive.sh
+++ roles/kiwix/files/create_zim_archive.sh
@@ -2,30 +2,24 @@
 
 OLD_PATH=/usr/local/share/kiwix/data
 
-ZIM_LIST=`find ./ -type f -regextype posix-extended -regex "^.*zim$|^.*zima{2}"`
-
-if [ -Z $ZIM_LIST ]; then
-        exit 0;
-fi
+apt-get install -y zip
 
 cd $OLD_PATH
 mkdir -p $OLD_PATH/packages/
 mkdir -p /var/cache/ideascube/catalog/packages
 
-echo "[+] Zim list..."
-echo "  --> $ZIM_LIST"
+find ./ -type f -regextype posix-extended -regex "^.*zim$|^.*zima{2}" -print0 | while IFS= read -r -d $'\0' i; do
 
-for i in $ZIM_LIST
-do
-        y=0
+        zim_file=`echo $i | cut -d "/" -f3`
 
-        echo "[+] Value of this entry $i"
+        echo "[+] Create archive for $i"
+
         new_name=`echo $i | cut -d "/" -f3 | cut -d "_" -f1-2 | sed 's/_/./'`
-        folder_name=`echo $i | cut -d "/" -f3 | cut -d "_" -f1-2 |
+        folder_name=`echo $i | cut -d "/" -f3 | cut -d "_" -f1-2`
         nameWithDate=$new_name-0000-00-00
 
         index_folder_full_path=`find ./ -type d -name "$folder_name*"`
-        index_folder=`find ./ -type d -name "$folder_name*"` -printf '%f\n'
+        index_folder=`find ./ -type d -name "$folder_name*" -printf '%f\n'`
 
         echo "$new_name" >> $OLD_PATH/list.txt
 
@@ -33,22 +27,22 @@ do
 
         is_several_files=`echo $i | grep zimaa`
 
-        if [ -n $is_several_files ]; then
+        if [ -n "$is_several_files" ]; then
                 cp ${i%?}* $OLD_PATH/packages/$nameWithDate/data/content/
         else
                 cp $i $OLD_PATH/packages/$nameWithDate/data/content/
         fi
 
         if [ -n $index_folder_full_path ]; then
-                cp $index_folder_full_path $OLD_PATH/packages/$nameWithDate/data/index
+                cp -r $index_folder_full_path $OLD_PATH/packages/$nameWithDate/data/index
 
                 cd $OLD_PATH/packages/$nameWithDate/data/library/
 
-                /usr/local/bin/kiwix-manage library.xml add ../content/$i -i=../index/$index_folder         
+                /usr/local/bin/kiwix-manage library.xml add ../content/$zim_file -i=../index/$index_folder         
         else
                 cd $OLD_PATH/packages/$nameWithDate/data/library/
 
-                /usr/local/bin/kiwix-manage library.xml add ../content/$i
+                /usr/local/bin/kiwix-manage library.xml add ../content/$zim_file
         fi 
 
         cd $OLD_PATH/packages/$nameWithDate/
@@ -56,7 +50,6 @@ do
 
         mv "$new_name-0000-00-00" /var/cache/ideascube/catalog/packages/$new_name-0000-00-00
         cd $OLD_PATH
-
 done
 
 rm -rf $OLD_PATH/packages
