b97c82cf5ffde9c4957d659cc8960e8f165cee82
steven <steven.wallimann@bibliosansfrontieres.org>
2017-02-06 18:10:57 +0100

    Adding some comments

--- roles/captive_portal/files/cap.py
+++ roles/captive_portal/files/cap.py
@@ -3,11 +3,10 @@ import cgi
 import os, subprocess
 
 URL_REDIR="http://ideasbox.lan"
-#REMOTE_IP="10.10.10.10"
 
 def allow_host(ipaddress):
 	"""
-	Allow host to bypass ideascube server
+	Allow host ip to bypass ideascube server
 	"""
 	cmd = "sudo iptables -t nat -I CAPTIVE_PASSLIST 1 -s {ip} -j ACCEPT".format(ip=ipaddress)
 	subprocess.check_output(cmd, shell=True)
@@ -15,6 +14,9 @@ def allow_host(ipaddress):
 
 REMOTE_IP=cgi.escape(os.environ["REMOTE_ADDR"])
 allow_host(REMOTE_IP)
+
+## Send HTML redirect to forward user to ideasbox.lan/koombook.lan
+
 print ("Content-type:text/html\r\n\r\n")
-print ("""<html><head><meta http-equiv="refresh" content="2; URL='%s'" />Going to ideasbox.lan</head></html>""" % URL_REDIR)
+print ("""<html><head><meta http-equiv="refresh" content="2; URL='%s'" />Going to %s</head></html>""" % (URL_REDIR,URL_REDIR))
 
