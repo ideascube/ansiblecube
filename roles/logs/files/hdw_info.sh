9926d5e38f7787f85ca5b26b667529724b6961d2
fheslouin <florian.heslouin@bibliosansfrontieres.org>
2017-02-08 15:43:29 +0100

    Pick up logs role from oneUpdateFile for an update version on master branch

--- roles/logs/files/hdw_info.sh
+++ roles/logs/files/hdw_info.sh
@@ -51,3 +51,11 @@ echo "#          Public IP address     #" >> $FILE
 echo "##################################" >> $FILE
 wget -qO- http://ipecho.net/plain         >> $FILE
 echo                                      >> $FILE
+echo "##################################" >> $FILE
+echo "#          Zim files             #" >> $FILE
+echo "##################################" >> $FILE
+find / -type f -regextype posix-extended -regex "^.*zim$|^.*zima{2}" -print0 -printf "\n"
+echo "##################################" >> $FILE
+echo "#          Zim index dir         #" >> $FILE
+echo "##################################" >> $FILE
+find / -type d -regextype posix-extended -regex "^.*idx" -print0 -printf "\n"
