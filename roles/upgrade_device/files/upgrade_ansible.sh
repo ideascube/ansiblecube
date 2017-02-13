421573c699228fd662fe323cedb140d360c1dfca
fheslouin <florian.heslouin@bibliosansfrontieres.org>
2017-02-13 11:25:57 +0100

    libssl-dev is required by pycrypto for Ansible 2.2

--- roles/upgrade_device/files/upgrade_ansible.sh
+++ roles/upgrade_device/files/upgrade_ansible.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install libffi-dev needed by Ansible 2.0
-apt-get install -y libffi-dev
+apt-get install -y libffi-dev libssl-dev
 
 # Fix broken PIP, ignore if requests folder does not exist
 mv /usr/local/lib/python2.7/dist-packages/requests /usr/local/lib/python2.7/dist-packages/requests.old/
