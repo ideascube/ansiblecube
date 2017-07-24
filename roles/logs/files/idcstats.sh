0e305442201cc8621d89a515ced67fdb686a41ab
ToM <thomas.faucher@bibliosansfrontieres.org>
2017-07-24 18:17:44 +0300

    Add the idcstats script to the logs/ role.
    This script sends some basic stats from ideascube to syslog, 4 times a day.
    The output is quite easy to parse using awk for example. The syslog
    facility is used so we don't need to take care of a new file.

---
+++ roles/logs/files/idcstats.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# idcstats -- quick'n'dirty stats from ideascube
+# Don't edit this script localy, it will be overwritten by ansiblecube anyway.
+
+
+#
+# conf / vars
+#
+
+DB=/var/ideascube/main/default.sqlite
+
+
+
+#
+# functions
+#
+
+# squery -- queries the sqlite database
+#   Usage: squery $query
+squery() {
+    sqlite3 $DB "$@"
+}
+
+# sqcount -- counts entries from a table
+#   Usage: sqcount $table
+sqcount() {
+    squery "select count(*) from $1"
+}
+
+
+
+#
+# main
+#
+
+users=$(  sqcount ideascube_user )
+medias=$( sqcount mediacenter_document )
+tags=$(   sqcount taggit_tag )
+books=$(  sqcount library_book )
+blog=$(   sqcount blog_content )
+
+# send this to syslog - no need of a separated file, use grep/awk/whatever
+logger --tag idcstats "$users users, $medias medias, $tags tags, $blog blogposts, $books books"
