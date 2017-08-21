5eccfb941a074e365d35c7682fc5ac480ca658b0
ToM <thomas.faucher@bibliosansfrontieres.org>
2017-08-21 16:23:58 +0200

    Absolute path for binaries ran from cron.

--- roles/captive_portal/files/clear_iptables.sh
+++ roles/captive_portal/files/clear_iptables.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 ## Get list of authorized IP
-passlist=$(iptables -t nat -nL CAPTIVE_PASSLIST | grep ACCEPT | awk '{print $4}')
+passlist=$(/sbin/iptables -t nat -nL CAPTIVE_PASSLIST | grep ACCEPT | awk '{print $4}')
 
 for ip in $passlist
 do
 	## Count ESTABLISHED connections from/to $ip
 
-	is_connected=`conntrack -L | grep $ip | grep ESTABLISHED | wc -l`
+	is_connected=`/sbin/conntrack -L | grep $ip | grep ESTABLISHED | wc -l`
 	
 	## If no connections then remove iptables rule to bypass redirection
 	if [ $is_connected -eq 0 ]
