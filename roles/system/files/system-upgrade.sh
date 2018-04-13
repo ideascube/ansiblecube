284dba94b59712dca9b5c9842be37c4d5df52dd9
fheslouin <florian.heslouin@bibliosansfrontieres.org>
2018-04-13 11:54:40 +0200

    Fetch only the first 2 numbers of ansible version

--- roles/system/files/system-upgrade.sh
+++ roles/system/files/system-upgrade.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-ansible_version=`pip freeze | grep ansible= | cut -d "=" -f3`
+ansible_version=`pip freeze | grep ansible=`
 
-if [ "$ansible_version" != "2.2.0.0" ]
+if [ "${ansible_version:9:3}" != "2.2" ]
 then
 	pip install -U ansible==2.2.0.0
 fi
